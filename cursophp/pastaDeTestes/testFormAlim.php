94b5fd017cba667368ac90629255a5d28cd4bc66
Lucas Ciacci <lucas.ciacci@alunos.unis.edu.br>
2024-05-09 15:00:01 -0300

    Forma Correta Form.Retroalimentado

--- cursophp/pastaDeTestes/testFormAlim.php
+++ cursophp/pastaDeTestes/testFormAlim.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
@@ -18,11 +18,42 @@
     <main>
         <p>
             <?php 
-                $nome = $_GET["nome"] ?? "<strong>desconhecido</strong>";
-                echo "O seu nome é <strong>$nome</strong>"; 
-                ?>
+                // $nome = $_GET["nome"] ?? "<strong>desconhecido</strong>";
+                // echo "O seu nome é <strong>$nome</strong>"; 
+            ?>
        </p>
    </main>
+</body>
+</html> -->
+
+
+<!--FORMA CORRETA-->
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário Retroalimentado</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php 
+        $nome = $_GET["nome"] ?? "desconhecido";
+    ?>
+    <main>
+        <h1>Formulário Retroalimentado</h1>
+        <form action="<?= $_SERVER['PHP_SELF']; ?>" method="get">
+            <label for="nome">Nome</label>
+            <input type="text" name="nome" id="idnome" value="<?= $nome ?>">
+            <input type="submit" value="Enviar">
+        </form>
+    </main>
+    <section>
+        <?php 
+            echo "O nome é $nome";
+        ?>
+    </section>
 </body>
 </html>
                 
